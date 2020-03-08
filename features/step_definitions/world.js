0a37dc6ce76e856445d6fea4035523ac3a3d6efe
Karen Botelho <neyde.botelho@solutis.com.br>
2020-03-08 18:25:50 -0300

    Trying to get the price

--- features/step_definitions/world.js
+++ features/step_definitions/world.js
@@ -2,6 +2,8 @@ const { setWorldConstructor, setDefaultTimeout } = require("cucumber");
 const { expect } = require("chai");
 const puppeteer = require("puppeteer");
 
+const delay = duration => new Promise(resolve => setTimeout(resolve, duration));
+
 var nameBookChose = "";
 
 class CustomWorld {
@@ -38,23 +40,30 @@ class CustomWorld {
   }
 
   async selectBook() {
-    await this.page.waitForSelector(
-      ".a-size-base-plus a-color-base a-text-normal"
-    );
+    var text;
+    await this.page.waitForSelector("#twotabsearchtextbox");
+
+    // ToDo discover how I can get the name of the book
     nameBookChose = await this.page.content(
-      (".a-size-base-plus a-color-base a-text-normall", text, { delay: 10000 })
+      (".a-link-normal a-text-normal", text, { delay: 10000 })
     );
-    await this.page.click(".a-size-base-plus a-color-base a-text-normal");
+
+    const selectElemBook = await this.page.$$(".a-offscreen");
+    // ToDo a foreach to discover what has the minor price to click
+
+    // ToDo discover how I can click on the book price
+    await selectElemBook.click(".a-price-whole");
   }
 
   async verifyBookName() {
-    await this.page.waitForSelector(".panel_content");
+    var text;
+    await this.page.waitForSelector("#productTitle");
 
     const content = await this.page.content(
-      (".panel_content", text, { delay: 10000 })
+      ("#productTitle", text, { delay: 10000 })
     );
 
-    if (!content.includes(nameBookChose))
+    if (!content.includes(text))
       throw new Error(
         `Expected page to contain text: ${nameBookChose}, but page contains only: ${content}`
       );
